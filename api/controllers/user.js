41c39b5597ba6b9f50dec7070ab68c933b65f0b6
Jugal Bhatt <jugalbhatt3@gmail.com>
2021-04-03 20:13:28 +0530

    add user routes

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -12,8 +12,57 @@ exports.update = async (req, res) => {
       });
     })
     .catch((e) => {
-      res.status(400).json({
+      res.status(500).json({
         error: e.toString(),
       });
     });
 };
+
+
+exports.getProfile = async (req, res)=>{
+  const { userId } = req.user;
+  let user = await User.aggregate(
+    [
+      {
+        $match : { _id: { $eq: mongoose.Types.ObjectId(userId) } },
+      },
+      {
+        $lookup: {
+          from: "teams",
+          localField: "team",
+          foreignField: "_id",
+          as: "team",
+        },
+      },
+      {
+        $project: {
+          _id: 1,
+          name: 1,
+          email: 1,
+          mobile: 1,
+          college: 1,
+          avatar: 1,
+          bio: 1,
+          "team._id": 1,
+          "team.name": 1,
+          "team.code": 1,
+        },
+      },
+    ]
+  )
+    if(user){
+      user = Object(user[0])
+      if(user.team && user.team.length >=1){
+        user.team = user.team[0]
+      }
+      res.status(200).json({
+        success: true,
+        user
+      })
+    }else{
+      req.status(404).json({
+        success: false,
+        message: "Does not exist"
+      })
+    }
+}
