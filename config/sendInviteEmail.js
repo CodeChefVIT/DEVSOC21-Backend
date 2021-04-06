e02087ee50857962bdb14b98bbc721b611d95958
Jugal Bhatt <jugalbhatt3@gmail.com>
2021-04-06 13:11:46 +0530

    add email invite

---
+++ config/sendInviteEmail.js
@@ -0,0 +1,303 @@
+exports.sendInvite = (name, link, code) => {
+  return `<!doctype html>
+  <html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml"
+      xmlns:o="urn:schemas-microsoft-com:office:office">
+  
+  <head>
+      <title>DEVSOC 21 | Team Join Invite</title>
+      <!--[if !mso]><!-- -->
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <!--<![endif]-->
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <style type="text/css">
+          #outlook a {
+              padding: 0;
+          }
+  
+          body {
+              margin: 0;
+              padding: 0;
+              -webkit-text-size-adjust: 100%;
+              -ms-text-size-adjust: 100%;
+          }
+  
+          table,
+          td {
+              border-collapse: collapse;
+              mso-table-lspace: 0pt;
+              mso-table-rspace: 0pt;
+          }
+  
+          img {
+              border: 0;
+              height: auto;
+              line-height: 100%;
+              outline: none;
+              text-decoration: none;
+              -ms-interpolation-mode: bicubic;
+          }
+  
+          p {
+              display: block;
+              margin: 13px 0;
+          }
+      </style>
+      <!--[if mso]>
+          <xml>
+          <o:OfficeDocumentSettings>
+            <o:AllowPNG/>
+            <o:PixelsPerInch>96</o:PixelsPerInch>
+          </o:OfficeDocumentSettings>
+          </xml>
+          <![endif]-->
+      <!--[if lte mso 11]>
+          <style type="text/css">
+            .mj-outlook-group-fix { width:100% !important; }
+          </style>
+          <![endif]-->
+      <style type="text/css">
+          @media only screen and (min-width:480px) {
+              .mj-column-per-100 {
+                  width: 100% !important;
+                  max-width: 100%;
+              }
+          }
+      </style>
+      <style type="text/css">
+          @media only screen and (max-width:480px) {
+              table.mj-full-width-mobile {
+                  width: 100% !important;
+              }
+  
+              td.mj-full-width-mobile {
+                  width: auto !important;
+              }
+          }
+      </style>
+  </head>
+  
+  <body style="background-color:#090021;">
+      <div style="background-color:#090021;">
+          <!--[if mso | IE]>
+        <table
+           align="center" border="0" cellpadding="0" cellspacing="0" class="" style="width:600px;" width="600"
+        >
+          <tr>
+            <td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;">
+        <![endif]-->
+          <div style="background:#090021;background-color:#090021;margin:0px auto;max-width:600px;">
+              <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation"
+                  style="background:#090021;background-color:#090021;width:100%;">
+                  <tbody>
+                      <tr>
+                          <td
+                              style="direction:ltr;font-size:0px;padding:20px 0;padding-bottom:0px;padding-top:30px;text-align:center;">
+                              <!--[if mso | IE]>
+                    <table role="presentation" border="0" cellpadding="0" cellspacing="0">
+                  
+          <tr>
+        
+              <td
+                 class="" style="vertical-align:top;width:600px;"
+              >
+            <![endif]-->
+                              <div class="mj-column-per-100 mj-outlook-group-fix"
+                                  style="font-size:0px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;">
+                                  <table border="0" cellpadding="0" cellspacing="0" role="presentation"
+                                      style="vertical-align:top;" width="100%">
+                                      <tr>
+                                          <td align="center"
+                                              style="font-size:0px;padding-bottom:15px;word-break:break-word;">
+                                              <table border="0" cellpadding="0" cellspacing="0" role="presentation"
+                                                  style="border-collapse:collapse;border-spacing:0px;">
+                                                  <tbody>
+                                                      <tr>
+                                                          <td> <a href="https://hackwith.codechefvit.com" target="_self">
+  
+                                                                  <img height="auto"
+                                                                      src="https://i.ibb.co/YLsVQZr/Header.png"
+                                                                      style="border:0;display:block;outline:none;text-decoration:none;height:auto;width:100%;font-size:13px;"
+                                                                      width="250" />
+  
+                                                              </a> </td>
+                                                      </tr>
+                                                  </tbody>
+                                              </table>
+                                          </td>
+                                      </tr>
+                                  </table>
+                              </div>
+                              <!--[if mso | IE]>
+              </td>
+            
+          </tr>
+        
+                    </table>
+                  <![endif]-->
+                          </td>
+                      </tr>
+                  </tbody>
+              </table>
+          </div>
+          <!--[if mso | IE]>
+            </td>
+          </tr>
+        </table>
+        
+        <table
+           align="center" border="0" cellpadding="0" cellspacing="0" class="" style="width:600px;" width="600"
+        >
+          <tr>
+            <td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;">
+        <![endif]-->
+  
+          <div style="background:#090021;background-color:#090021;margin:0px auto;max-width:600px;">
+              <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation"
+                  style="background:#090021;background-color:#090021;width:100%;">
+                  <tbody>
+                      <tr>
+                          <td
+                              style="direction:ltr;font-size:0px;padding:20px 0;padding-bottom:0px;padding-top:0px;text-align:center;">
+                              <!--[if mso | IE]>
+                    <table role="presentation" border="0" cellpadding="0" cellspacing="0">
+                  
+          <tr>
+        
+              <td
+                 class="" style="vertical-align:top;width:600px;"
+              >
+            <![endif]-->
+                              <div class="mj-column-per-100 mj-outlook-group-fix"
+                                  style="font-size:0px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;">
+                                  <table border="0" cellpadding="0" cellspacing="0" role="presentation"
+                                      style="vertical-align:top;" width="100%">
+                                      <tr>
+                                          <td align="left"
+                                              style="font-size:0px;padding:10px 25px;padding-top:30px;padding-right:40px;padding-bottom:20px;padding-left:40px;word-break:break-word;">
+                                              <div
+                                                  style="font-family: Montserrat;font-size:13px;line-height:22px;text-align:left;color:#55575d;">
+                                                  <p style="margin: 10px 0;font-size:15px; color:#151e23; font-family:Montserrat; color:#ffffff"
+                                                      align="left">
+                                                      Hi there,
+                                                      <br /><br />
+                                                      <span style="color: #6E5AFC">${name}</span> has
+                                                      invited you
+                                                      to join his/her team in
+                                                      -
+                                                      <span style="color: #3054FE">DEVSOC 21</span>.
+                                                      <br /><br />
+                                                      <span style="color: #f3f3f3">If you accept the request, you will be
+                                                          added to their team.
+                                                          <br /><br />
+                                                          <h1 style="color: #3054FE;">About DEVSOC</h1>
+                                                          <span style="color: #f3f3f3">You can read more about the
+                                                              hackathon</span> <a href="https://devsoc.codechefvit.com"
+                                                              style="color: #6E5AFC">here</a>.
+                                                          <br />
+                                                      </span>
+                                                      <br /><br />
+                                                      <center style="color: #f3f3f3;">
+                                                          <a
+                                                              href="${link}"><button
+                                                                  style="background: #6E5AFC;color: white;padding: 15px;border: 1px solid #3054FE;border-radius: 5px;cursor: pointer;">Accept
+                                                                  Invite</button></a>
+                                                          <br /><br />
+                                                          (or)
+                                                          <br /><br />
+                                                          <span style="font-size: 18px;">Use this team code: </span><span
+                                                              style="color: #3054FE;font-size: 18px;">${code}</span>
+                                                      </center>
+                                                  </p>
+                                              </div>
+                                          </td>
+                                      </tr>
+                                  </table>
+                              </div>
+                              <!--[if mso | IE]>
+              </td>
+            
+          </tr>
+        
+                    </table>
+                  <![endif]-->
+                          </td>
+                      </tr>
+                  </tbody>
+              </table>
+          </div>
+          <!--[if mso | IE]>
+            </td>
+          </tr>
+        </table>
+        
+        <table
+           align="center" border="0" cellpadding="0" cellspacing="0" class="" style="width:600px;" width="600"
+        >
+          <tr>
+            <td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;">
+        <![endif]-->
+          <div style="background:#090021;background-color:#090021;margin:0px auto;max-width:600px;">
+              <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation"
+                  style="background:#090021;background-color:#090021;width:100%;">
+                  <tbody>
+                      <tr>
+                          <td
+                              style="direction:ltr;font-size:0px;padding:20px 0;padding-bottom:0px;padding-top:0px;text-align:center;">
+                              <!--[if mso | IE]>
+                    <table role="presentation" border="0" cellpadding="0" cellspacing="0">
+                  
+          <tr>
+        
+              <td
+                 class="" style="vertical-align:top;width:600px;"
+              >
+            <![endif]-->
+                              <div class="mj-column-per-100 mj-outlook-group-fix"
+                                  style="font-size:0px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;">
+                                  <table border="0" cellpadding="0" cellspacing="0" role="presentation"
+                                      style="vertical-align:top;" width="100%">
+                                      <tr>
+                                          <td align="center"
+                                              style="font-size:0px;padding:10px 25px;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px;word-break:break-word;">
+                                              <table border="0" cellpadding="0" cellspacing="0" role="presentation"
+                                                  style="border-collapse:collapse;border-spacing:0px;">
+                                                  <tbody>
+                                                      <tr>
+                                                          <td style="width:600px;"> <a href="#" target="_self">
+  
+                                                                  <img height="auto"
+                                                                      src="https://i.ibb.co/gSLMkh8/Footer.png"
+                                                                      style="border:0;display:block;outline:none;text-decoration:none;height:auto;width:100%;font-size:13px;"
+                                                                      width="600" />
+  
+                                                              </a> </td>
+                                                      </tr>
+                                                  </tbody>
+                                              </table>
+                                          </td>
+                                      </tr>
+                                  </table>
+                              </div>
+                              <!--[if mso | IE]>
+              </td>
+            
+          </tr>
+        
+                    </table>
+                  <![endif]-->
+                          </td>
+                      </tr>
+                  </tbody>
+              </table>
+          </div>
+          <!--[if mso | IE]>
+            </td>
+          </tr>
+        </table>
+        <![endif]-->
+      </div>
+  </body>
+  
+  </html>`
+}
