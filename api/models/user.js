0f5ec7fce69d2bcba58dfcaad0bb7428ecab2957
Shivam Mehta <50650500+N0v0cain3@users.noreply.github.com>
2021-04-09 10:13:17 +0530

    Revert "even even even "

--- api/models/user.js
+++ api/models/user.js
@@ -48,14 +48,6 @@ const userSchema = mongoose.Schema(
     isCheckedIn: { type: Boolean },
 
     inTeam: { type: Boolean, default: false },
-
-    numOtpLogins: { type: Number, default: 0 },
-
-    otpTimestamp: { type: Date },
-
-    otpExpiryTimestamp: { type: Date },
-
-    currentOtp: { type: String, default: 0 },
   },
   { timestamps: true }
 );
