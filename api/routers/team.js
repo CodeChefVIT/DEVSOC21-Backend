801f9c07008212c7d60e3878d94372cb1bad27b1
jugaldb <jugalbhatt3@gmail.com>
2022-03-03 01:26:30 +0530

    not

--- api/routers/team.js
+++ api/routers/team.js
@@ -24,7 +24,7 @@ router.get("/user", checkAuth, team.getTeamByUser);
 
 router.post("/removeUser", checkAuth, recaptchaMiddleware, team.removeUser);
 
-// router.post('/saveIdea', checkAuth, team.saveIdea);
+router.post('/saveIdea', checkAuth, team.saveIdea);
 
 // router.post('/saveIdea', checkAuth, team.saveIdea);
 
